8fb32b036a6594122f09ee42f779c0ee72266a0e
HainsDolichan <83398614+HainsDolichan@users.noreply.github.com>
2021-06-07 07:12:10 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,20 @@
+import './App.css';
+import React from "react";
+import Header from './Header';
+import Footer from './Footer';
+import Notes from './Notes';
+
+function App() {
+  return (<div>
+    <Header />
+    <Footer />
+    <Notes />
+    <Notes />
+    <Notes />
+    <Notes />
+    
+    </div>
+  );
+}
+
+export default App;
---
+++ Notes.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Notes(){
+    return(
+        <div class="note">
+            <h5>Javascript and React.js</h5>
+            <p>
+                this was an amazing bootcamp taken up by Shaurya Sinha.
+                We covered everything from scarch including Javascript,React.js,HTML.
+            </p>
+        </div>
+            
+    );
+}
+
+export default Notes;
